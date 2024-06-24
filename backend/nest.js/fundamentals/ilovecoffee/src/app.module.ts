a063e4321d2fa68225009c30ae12dcdd00c0648e
Abrahán Mesa <ab.mesa@icloud.com>
2024-06-24 10:38:43 +0200

    add database

--- backend/nest.js/fundamentals/ilovecoffee/src/app.module.ts
+++ backend/nest.js/fundamentals/ilovecoffee/src/app.module.ts
@@ -1,11 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CoffeesController } from './coffees/coffees.controller';
+import { CoffeesModule } from './coffees/coffees.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController, CoffeesController],
+  imports: [
+    CoffeesModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres', // type of our database
+      host: 'localhost', // database host
+      port: 5432, // database host
+      username: 'user', // username
+      password: 'password', // user password
+      database: 'ilovecoffee', // name of our database,
+      autoLoadEntities: true, // models will be loaded automatically
+      synchronize: true, // your entities will be synced with the database(recommended: disable in prod)
+    }),
+  ],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
--- backend/nest.js/fundamentals/ilovecoffee/src/coffees/coffees.controller.ts
+++ backend/nest.js/fundamentals/ilovecoffee/src/coffees/coffees.controller.ts
@@ -1,29 +1,42 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { CoffeesService } from './coffees.service';
+import { CreateCoffeeDto } from './dto/create-coffee.dto';
+import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 
 @Controller('coffees')
 export class CoffeesController {
+  constructor(private readonly coffeesService: CoffeesService) {}
+
   @Get()
   findAll() {
-    return 'This action returns all the coffees';
+    return this.coffeesService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return `This action returns #${id} coffee`;
+  findOne(@Param('id') id: number) {
+    return this.coffeesService.findOne(id);
   }
 
   @Post()
-  create(@Body() body: { name: string }) {
-    return body.name;
+  create(@Body() createCoffeeDto: CreateCoffeeDto) {
+    return this.coffeesService.create(createCoffeeDto);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() body: { name: string }) {
-    return `This action updates coffee #${id} with ${body.name}`;
+  update(@Param('id') id: number, @Body() updateCoffeeDto: UpdateCoffeeDto) {
+    return this.coffeesService.update(id, updateCoffeeDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return `This action removes coffee #${id}`;
+  remove(@Param('id') id: number) {
+    return this.coffeesService.remove(id);
   }
 }
